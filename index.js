13325c94deedf7ed6db2d06556103cd620df9262
ricardo-ljr <ricardo.leitejr95@gmail.com>
2020-03-13 11:10:40 -0600

    Working on Deploying to Heroku

--- index.js
+++ index.js
@@ -13,10 +13,12 @@ I need this code, but don't know where, perhaps should make some middleware, don
 Go code!
 */
 
+require("dotenv").config();
+
 const express = require("express");
 
 const server = require("./server");
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
   console.log(`\n* listening on http://localhost:${port} *\n`);
